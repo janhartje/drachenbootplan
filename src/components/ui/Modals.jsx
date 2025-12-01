e8434afbe93d2ad2d1482d45c8ff1b1051232131
janhartje <jan@janhartje.com>
2025-12-01 21:53:18 +0100

    chore: remove OnboardingModal from Modals.jsx exports.

--- src/components/ui/Modals.jsx
+++ src/components/ui/Modals.jsx
@@ -1,12 +1,10 @@
 import AddGuestModal from './modals/AddGuestModal';
-import OnboardingModal from './modals/OnboardingModal';
 import HelpModal from './modals/HelpModal';
 import ImprintModal from './modals/ImprintModal';
 import ChangelogModal from './modals/ChangelogModal';
 
 export {
   AddGuestModal,
-  OnboardingModal,
   HelpModal,
   ImprintModal,
   ChangelogModal
